552f0b957d40e1e806d8e8398dae573783fcab26
Umar Hassan Khan <umar.hassan.cowlar@gmail.com>
2023-07-06 11:42:27 +0500

    Added weather route authorization

--- src/routes/weatherRoute.js
+++ src/routes/weatherRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
+const weatherAuthorization = require("../middlewares/authorization/weatherAuthorization");
 // importing controllers
 const weatherController = require("../controllers/weatherController");
 
 const router = express.Router();
 
 // get all data
-router.get("/all", weatherController.getAllData);
+router.get("/all", weatherAuthorization, weatherController.getAllData);
 
 // get data within range
 router.get("/ranged", weatherController.getRangedData);
