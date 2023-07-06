4260ae750ba0bc68f326c5f1f4839f804efbc250
Umar Hassan Khan <umar.hassan.cowlar@gmail.com>
2023-07-06 09:33:51 +0500

    Minimum password length set to 6

--- src/middlewares/validations/userSchema.js
+++ src/middlewares/validations/userSchema.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const userSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().required().min(6),
 });
 
 module.exports = userSchema;
